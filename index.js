2a4d31494dde4b7a1f9585b30fb7ea441f1b9893
Ian Del Duca <ian@gnar.dog>
2024-06-01 15:20:53 -0400

    Add timeout in order wait for AI response to come back

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const listLength = document.querySelectorAll('[role="list"]').length;
+const visitAILinks = () => {
+  const listLength = document.querySelectorAll('[role="list"]').length;
 
-const links =
-  document.querySelectorAll('[role="list"]')[2]?.children ||
-  document.querySelectorAll('[role="list"]').item(listLength - 1).children;
+  const links =
+    document.querySelectorAll('[role="list"]')[2]?.children ||
+    document.querySelectorAll('[role="list"]').item(listLength - 1).children;
 
-for (let i = 0; i < links.length; i++) {
-  const nodeList = [];
+  for (let i = 0; i < links.length; i++) {
+    const nodeList = [];
 
-  nodeList.push(links.item(i));
-  const anchors = nodeList.map((node, i) => {
-    return (
-      node.children[0].children[0].children[0].children[0].children[0] ||
-      undefined
-    );
-  });
-
-  if (anchors) {
-    anchors.forEach((anchor) => {
-      console.log({ href: anchor.href });
-      if (anchor.href) {
-        window.open(anchor.href, "_blank");
-      }
+    nodeList.push(links.item(i));
+    const anchors = nodeList.map((node, i) => {
+      return (
+        node.children[0].children[0].children[0].children[0].children[0] ||
+        undefined
+      );
     });
+
+    if (anchors) {
+      anchors.forEach((anchor) => {
+        console.log({ href: anchor.href });
+        if (anchor.href) {
+          window.open(anchor.href, "_blank");
+        }
+      });
+    }
   }
-}
+};
+
+setTimeout(visitAILinks, 3000);
